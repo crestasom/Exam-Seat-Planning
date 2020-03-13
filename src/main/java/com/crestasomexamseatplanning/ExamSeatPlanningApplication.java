7768a0234729edb8451bc506ef37b21d31588c3e
som <shresthasom@saipal.org>
2020-03-13 14:42:51 +0545

    init repo

---
+++ src/main/java/com/crestasomexamseatplanning/ExamSeatPlanningApplication.java
@@ -0,0 +1,13 @@
+package com.crestasomexamseatplanning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamSeatPlanningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamSeatPlanningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crestasomexamseatplanning/ExamSeatPlanningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crestasomexamseatplanning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamSeatPlanningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
